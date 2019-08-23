98ee985b9933eb1d529527d120d3dd70ec46fbb9
Sara McCombs <thesaramccombs@gmail.com>
2019-08-23 00:13:38 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,7 @@
-
+# method takes in an argument of a collection 
+# method iterates over that collection using a while loop
+# method executes the code in the block for each element in the collection 
+# method uses the yield keyword
+# method returns a modified collection
+# method named 'my_collect'
 
