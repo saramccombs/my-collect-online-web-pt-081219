6d25fc3e96ec282d2c5638d1440f4a4fdecaa4bd
Sara McCombs <thesaramccombs@gmail.com>
2019-08-23 00:23:12 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+# method takes in an argument of a collection 
+# method iterates over that collection using a while loop
+# method executes the code in the block for each element in the collection 
+# method uses the yield keyword
+# method returns a modified collection
+# method named 'my_collect'
 
-
+def my_collect(collection)
+  i = 0 
+  new_collection = []
+  while i < collection.length 
+    new_collection << yield(collection[i])
+    i += 1 
+  end
+  new_collection
+end
