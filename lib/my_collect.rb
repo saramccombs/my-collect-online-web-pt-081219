fac8eba1a14480fc0f7f99ba571ca76f5dc1c94f
Sara McCombs <thesaramccombs@gmail.com>
2019-08-23 00:16:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -7,8 +7,10 @@
 
 def my_collect(collection)
   i = 0 
+  new_collection = []
   while i < collection.length 
     yield(collection[i])
+    new_collection << collection[i]
     i += 1 
   end
 end
